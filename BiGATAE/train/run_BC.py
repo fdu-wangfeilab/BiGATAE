d2bc76c21ac072ec4f9d7496c52474860a1e2ce5
HickeyTao <149254050+HickeyTao@users.noreply.github.com>
2024-01-02 18:48:51 +0800

    Update run_BC.py

--- BiGATAE/train/run_BC.py
+++ BiGATAE/train/run_BC.py
@@ -1,50 +1,46 @@
-import scanpy as sc
-import numpy as np
-import pandas as pd
+import model
 import torch
-import torch.nn as nn
-from torch import optim
-import copy
-from igraph import Graph as IGraph
-
-from model import model
+import scanpy as sc
+import anndata as ad
+from dataloader import load_slices_fourST
 from utils.get_edge_index import get_edge_index
 from paste_ import get_pi
-from tist import create_similar_network0
-
+import numpy as np
 
 def intersect(lst1, lst2): 
-
     temp = set(lst2)
     print(len(temp))
     lst3 = [value for value in lst1 if value in temp]
     return lst3
 
-def load_slices_fourST(data_dir='/home/sxa/Datasets/breast_cancer_data/', slice_names=["slice1", "slice2", "slice3", "slice4"]):
-    slices = []  
-    for slice_name in slice_names:
-        print(slice_name)
-        slice_i = sc.read_csv(data_dir + slice_name + ".csv")
-        slice_i_coor = np.genfromtxt(data_dir + slice_name + "_coor.csv", delimiter = ',')
-        slice_i.obsm['spatial_coor'] = slice_i_coor
-        # Preprocess slices
-        sc.pp.filter_genes(slice_i, min_counts = 15)
-        sc.pp.filter_cells(slice_i, min_counts = 100)
-        slices.append(slice_i)
-    adata_layer_1, adata_layer_2, adata_layer_3, adata_layer_4 = slices
-    common_genes = intersect(adata_layer_1.var.index, adata_layer_2.var.index)  
-    common_genes = intersect(common_genes, adata_layer_3.var.index)
-    common_genes = intersect(common_genes, adata_layer_4.var.index)
-    adata_layer_1 = adata_layer_1[:, common_genes]
-    adata_layer_2 = adata_layer_2[:, common_genes]
-    adata_layer_3 = adata_layer_3[:, common_genes]
-    adata_layer_4 = adata_layer_4[:, common_genes]
-    slices = [adata_layer_1, adata_layer_2, adata_layer_3, adata_layer_4]
-    return slices
+# load BC data and get 
+slice1, slice2, slice3, slice4 = load_slices_fourST(data_dir='/home/sxa/Datasets/breast_cancer_data/', slice_names=["slice1", "slice2", "slice3", "slice4"])
 
-slices = load_slices_fourST()
-slice1, slice2, slice3, slice4 = slices
+slice1_slice2_pi = get_pi(slice1, slice2)
+slice1_slice2_pi_edge_index = get_edge_index(slice1_slice2_pi)
+torch.save(slice1_slice2_pi_edge_index, '/home/tyh/new_work/BiGATAE/tmp_data/BC_edges/slice1(2).pt')
 
+slice1_slice2_pi = get_pi(slice2, slice1)
+slice1_slice2_pi_edge_index = get_edge_index(slice1_slice2_pi)
+torch.save(slice1_slice2_pi_edge_index, '/home/tyh/new_work/BiGATAE/tmp_data/BC_edges/slice2(1).pt')
+
+slice1_slice2_pi = get_pi(slice2, slice3)
+slice1_slice2_pi_edge_index = get_edge_index(slice1_slice2_pi)
+torch.save(slice1_slice2_pi_edge_index, '/home/tyh/new_work/BiGATAE/tmp_data/BC_edges/slice2(3).pt')
+
+slice1_slice2_pi = get_pi(slice3, slice2)
+slice1_slice2_pi_edge_index = get_edge_index(slice1_slice2_pi)
+torch.save(slice1_slice2_pi_edge_index, '/home/tyh/new_work/BiGATAE/tmp_data/BC_edges/slice3(2).pt')
+
+slice1_slice2_pi = get_pi(slice3, slice4)
+slice1_slice2_pi_edge_index = get_edge_index(slice1_slice2_pi)
+torch.save(slice1_slice2_pi_edge_index, '/home/tyh/new_work/BiGATAE/tmp_data/BC_edges/slice3(4).pt')
+
+slice1_slice2_pi = get_pi(slice4, slice3)
+slice1_slice2_pi_edge_index = get_edge_index(slice1_slice2_pi)
+torch.save(slice1_slice2_pi_edge_index, '/home/tyh/new_work/BiGATAE/tmp_data/BC_edges/slice4(3).pt')
+
+#get enhance 
 # slice1
 slice1_spots_num = slice1.X.shape[0]
 slice2_spots_num = slice2.X.shape[0]
@@ -79,8 +75,6 @@ slice1.write_h5ad('./tmp_data/BC_enhanced_data/slice1(2).h5ad')
 
 
 # slice2(1)
-slices = load_slices_fourST()
-slice1, slice2, slice3, slice4 = slices
 
 slice1_spots_num = slice1.X.shape[0]
 slice2_spots_num = slice2.X.shape[0]
@@ -113,8 +107,6 @@ slice2.write_h5ad('./tmp_data/BC_enhanced_data/slice2(1).h5ad')
 
 
 # slice2(3)
-slices = load_slices_fourST()
-slice1, slice2, slice3, slice4 = slices
 
 slice3_spots_num = slice3.X.shape[0]
 slice2_spots_num = slice2.X.shape[0]
@@ -145,8 +137,6 @@ slice2.X=model_.encoder(slice1_slice2_pi_edge_index.to(device), slice2_X, slice3
 slice2.write_h5ad('./tmp_data/BC_enhanced_data/slice2(3).h5ad')
 
 # slice3(2)
-slices = load_slices_fourST()
-slice1, slice2, slice3, slice4 = slices
 
 slice3_spots_num = slice3.X.shape[0]
 slice2_spots_num = slice2.X.shape[0]
@@ -178,8 +168,6 @@ slice3.X=model_.encoder(slice1_slice2_pi_edge_index.to(device), slice3_X, slice2
 slice3.write_h5ad('./tmp_data/BC_enhanced_data/slice3(2).h5ad')
 
 # slice3(4)
-slices = load_slices_fourST()
-slice1, slice2, slice3, slice4 = slices
 
 slice3_spots_num = slice3.X.shape[0]
 slice4_spots_num = slice4.X.shape[0]
@@ -210,8 +198,6 @@ slice3.X=model_.encoder(slice1_slice2_pi_edge_index.to(device), slice3_X, slice4
 slice3.write_h5ad('./tmp_data/BC_enhanced_data/slice3(4).h5ad')
 
 # slice4(3)
-slices = load_slices_fourST()
-slice1, slice2, slice3, slice4 = slices
 
 slice3_spots_num = slice3.X.shape[0]
 slice4_spots_num = slice4.X.shape[0]
@@ -241,3 +227,4 @@ slice4.write_h5ad('./tmp_data/BC_data/slice4.h5ad')
 model_.eval()
 slice4.X=model_.encoder(slice1_slice2_pi_edge_index.to(device), slice4_X, slice3_X).detach().cpu().numpy()
 slice4.write_h5ad('./tmp_data/BC_enhanced_data/slice4(3).h5ad')
+
